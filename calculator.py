edd33d026c994720f7213e03f828ee5527367711
Jackie Contento <jmc161@duke.edu>
2020-08-24 13:06:16 -0400

    User can enter HDL test result in console

--- calculator.py
+++ calculator.py
@@ -10,4 +10,21 @@ def interface():
         elif choice == '1':
             HDL_driver()
    
-interface()
+def HDL_driver():
+    # get input
+    HDL_result = HLD_input_fun
+    # check if HDL is normal
+    check_HDL
+    # output
+    if check_HDL >= 60:
+        print("HDL is normal")
+    elif 40 <= check_HDL <60
+        print("HDL is borderline low")
+    else
+        print("HDL is low")
+
+def HDL_input_fun():
+        HDL_input = input("Enter the HDL test result: ")
+        return int(HDL_input)
+
+
