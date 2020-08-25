5ef1029d88c348723db0c6654d88c1832c289955
Jackie Contento <jmc161@duke.edu>
2020-08-24 20:34:00 -0400

    Added LDL options to blood calculator function

--- calculator.py
+++ calculator.py
@@ -3,20 +3,22 @@ def interface():
     while True:
         print("Options for you:")
         print("1 - HDL")
+        print("2 - LDL")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice == '9':
             return
         elif choice == '1':
             HDL_driver()
+        elif choice == '2':
+            LDL_driver()
+            
    
 def HDL_driver():
     # get input
     HDL_result = HDL_input_fun()
-
     # check if HDL is normal
     analyze_HDL_result = analyze_HDL_fun(HDL_result)
-
     # output
     output = output_HDL_analysis(HDL_result,analyze_HDL_result)
     
@@ -35,6 +37,32 @@ def analyze_HDL_fun(HDL_result):
 def output_HDL_analysis(HDL_result, analyze_HDL_result):
     print("The HDL result is {}".format(HDL_result))
     print("That is {}".format(analyze_HDL_result))
+
+def LDL_driver():
+    # get input
+    LDL_result = LDL_input_fun()
+    # check if HDL is normal
+    analyze_LDL_result = analyze_LDL_fun(LDL_result)
+    # output
+    output = output_LDL_analysis(LDL_result,analyze_LDL_result)
+    
+def LDL_input_fun():
+    LDL_input = input("Enter the LDL test result: ")
+    return int(LDL_input)
+
+def analyze_LDL_fun(LDL_result):
+    if LDL_result < 130:
+        return "Normal"
+    elif 130 <= LDL_result <= 159:
+        return "Borderline High"
+    elif 160 <= LDL_result <= 189:
+        return "High"
+    else:
+        return "Very High"
+
+def output_LDL_analysis(LDL_result, analyze_LDL_result):
+    print("The LDL result is {}".format(LDL_result))
+    print("That is {}".format(analyze_LDL_result))
 
 
 interface()
